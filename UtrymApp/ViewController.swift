b2928cfbe91feabfb05637e4dbf468443768e636
Alexis Barnique <alexisbarnique@hotmail.com>
2018-05-27 19:56:12 -0400

    Initial Commit

---
+++ UtrymApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UtrymApp
+//
+//  Created by Alexis Barniquez on 27/5/18.
+//  Copyright © 2018 Alexis Barniquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
