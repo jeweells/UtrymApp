26e65b38e45e79a657e44c25c72f0581e658ed3e
Alexis Barnique <alexisbarnique@gmail.com>
2018-07-28 13:38:30 -0400

    add feed collectionview to profile view

---
+++ PostProfileCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostProfileCell.swift
+//  UtrymApp
+//
+//  Created by Alexis Barniquez on 28/7/18.
+//  Copyright © 2018 Alexis Barniquez. All rights reserved.
+//
+
+import UIKit
+
+class PostProfileCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
