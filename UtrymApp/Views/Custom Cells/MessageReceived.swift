7aa5149fec6ddd798b0bb3586b7d4f46189c4839
Alfredo Rodriguez <enriquea.rodriguezr@gmail.com>
2018-07-29 16:31:21 -0500

    More chat progress

--- UtrymApp/Views/Custom Cells/MessageReceived.swift
+++ UtrymApp/Views/Custom Cells/MessageReceived.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class MessageReceived: UITableViewCell {
 
+    @IBOutlet weak var messageContainer: UIView!
+    @IBOutlet weak var messageText: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        messageContainer.roundedRightTopLeft()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
