09a3b81d54b0b663a68978d6a32f8ea78ec7cdcb
Alexis Barnique <alexisbarnique@gmail.com>
2018-07-31 22:41:52 -0400

    carga de nueva estructura del chat

--- UtrymApp/Models/ChatNew.swift
+++ UtrymApp/Models/ChatNew.swift
@@ -11,55 +11,17 @@ import Firebase
 import FirebaseDatabase
 
 class ChatNew {
+    var cliente: String
+    var estilista: String
+    var hora: NSNumber
+    var mensaje: String
+    var leidoCli: Bool = false
+    var leidoEst: Bool = false
     
-    var key: String?
-    let title: String
-    let memberHash: String
-    let memberUIDs: [String]
-    var lastMessage: String?
-    var lastMessageSent: Date?
-    
-    init?(snapshot: DataSnapshot) {
-        guard !snapshot.key.isEmpty,
-            let dict = snapshot.value as? [String : Any],
-            let title = dict["title"] as? String,
-            let memberHash = dict["memberHash"] as? String,
-            let membersDict = dict["members"] as? [String : Bool],
-            let lastMessage = dict["lastMessage"] as? String,
-            let lastMessageSent = dict["lastMessageSent"] as? TimeInterval
-            else { return nil }
-        
-        self.key = snapshot.key
-        self.title = title
-        self.memberHash = memberHash
-        self.memberUIDs = Array(membersDict.keys)
-        self.lastMessage = lastMessage
-        self.lastMessageSent = Date(timeIntervalSince1970: lastMessageSent)
-    }
-    
-    static func hash(forMembers members: [UserNew]) -> String {
-        guard members.count == 2 else {
-            fatalError("There must be two members to compute member hash.")
-        }
-        
-        let firstMember = members[0]
-        let secondMember = members[1]
-        
-        let memberHash = String(firstMember.uid.hashValue ^ secondMember.uid.hashValue)
-        return memberHash
-    }
-    
-    init(members: [UserNew]){
-        // 1
-        assert(members.count == 2, "There must be two members in a chat.")
-        
-        // 2
-        self.title = members.reduce("") { (acc, cur) -> String in
-            return acc.isEmpty ? cur.username : "\(acc), \(cur.username)"
-        }
-        // 3
-        self.memberHash = ChatNew.hash(forMembers: members)
-        // 4
-        self.memberUIDs = members.map { $0.uid }
+    init(clienteText: String, estilistaText: String, horaInt: NSNumber, mensajeText: String) {
+        cliente = clienteText
+        estilista = estilistaText
+        hora = horaInt
+        mensaje = mensajeText
     }
 }
